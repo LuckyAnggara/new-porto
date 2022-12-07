87661d8da3156216267129ffe674c4f01a5ebcbc
kerjaanlucky <kerjaanlucky@gmail.com>
2022-12-07 19:33:35 +0700

    edit

--- components/CallMe.vue
+++ components/CallMe.vue
@@ -3,7 +3,7 @@
     <div class="flex flex-col items-center">
       <div>
         <span class="tracking-normal font-bold text-3xl text-gray-700"
-          >Let's Work Together</span
+          >Get in Touch</span
         >
         <div class="flex flex-row p-6">
           <div class="border-t-4 basis-1/3"></div>
@@ -12,10 +12,15 @@
         </div>
       </div>
     </div>
-    <div class="flex justify-center mt-24">
-      <span
-        class="tracking-normal font-bold font-solways text-6xl text-gray-700"
-        >bug.rto@gmail.com</span
+    <div class="flex flex-col items-center mt-24">
+      <a class="w-2/6 text-center text-gray-700 text-xl mb-5">
+        I'm always open to discussing partnership opportunities or just want to
+        say hi, i'll try my best to get back to you
+      </a>
+      <a
+        class="inline-block px-7 py-3 bg-gray-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-gray-700 hover:shadow-lg focus:bg-gray-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-800 active:shadow-lg transition duration-150 ease-in-out"
+        href="mailto:bug.rto@gmail.com"
+        >Say Hello</a
       >
     </div>
   </div>
