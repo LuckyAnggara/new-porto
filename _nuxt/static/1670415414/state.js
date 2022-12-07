9ddd9e094f7f552f077a072d79de14649c323f8a
kerjaanlucky <kerjaanlucky@gmail.com>
2022-12-07 19:34:18 +0700

    Updates

--- _nuxt/static/1670415414/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnew-porto\u002F_nuxt\u002Fstatic\u002F1670415414",layout:"master",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fnew-porto\u002F",assetsPath:"\u002Fnew-porto\u002F_nuxt\u002F",cdnURL:a}}}}(null));
